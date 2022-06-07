e1151ddb40435c9db3d3b625416d82cd3c6bd700
kikeDev <kdtl2hs@gmail.com>
2022-06-07 14:20:14 -0500

    ejercicios 2-5

--- src/index.js
+++ src/index.js
@@ -1,27 +1,107 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client'
-import './index.css';
-import Header from './components/Header'
-import Content from './components/Content'
-import Total from './components/Total'
-const container = document.getElementById('root')
-const root = createRoot(container)
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
 
+const container = document.getElementById("root");
+const root = createRoot(container);
+/*
+const parts = [
+  {
+    name: 'Fundamentals of React',
+    exercises: 10
+  },
+  {
+    name: 'Using props to pass data',
+    exercises: 7
+  },
+  {
+    name: 'State of a component',
+    exercises: 14
+  }
+]
 
+const partsName = parts.map(value => value.name )
+const partsNumber = parts.map(value => value.exercises)
+*/
 const App = () => {
-  
-  
-  return (
-    
-    <div>
-     <Header/>
-     <Content/>
-     <Total/>
-     
-    
-    </div>
-  )
+  const course = {
+    name: 'Web languages',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+  const names = course.parts.map(value => value.name )
+  const total = course.parts.map(value => value.exercises )
+  return (  
+      <div>
+    <Header course={course.name} />
+    <Content partsname={names} />
+    <Total partsnumb={total} />
+  </div>)
+
 }
 
+const Header = (props) =>{
+  
+  return(<div>
+  <h1>{props.course}</h1>
+  
+<p></p>
+</div>)
+}
+
+const Content = (props) =>{
  
-root.render(<App />)
+  return(<div>
+        <h1>{props.partsname}</h1>
+  
+</div>)
+}
+
+const Total = (props)=>{
+ 
+  return(<div>
+    
+    {props.partsnumb}
+  
+</div>)
+}
+
+/*const Part1 = (props) =>{
+  return(
+  <p> {props.part1}   </p>
+  )}
+const Part2 = (props) =>{
+  return(
+  <p>{props.part2} &nbsp; : &nbsp;  {props.exercises2}  </p>
+)}
+const Part3 = (props) =>{
+  return(
+  <p>{props.part3} &nbsp; : &nbsp;  {props.exercises3}  </p>
+)}
+
+
+const Total = () =>{
+ 
+  let result = ( {exercises1} + {exercises2} + {exercises3})
+ return( <div>
+
+   <h1>Resultado: &nbsp; {result}</h1>
+ 
+
+
+</div>)
+}
+*/
+root.render(<App />);
